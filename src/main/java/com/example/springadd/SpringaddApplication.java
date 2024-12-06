6ae71214a182d6afb695e61407e21b72952462de
HongSaek-Ko <bh1152@naver.com>
2024-12-06 10:45:49 +0900

    최초 커밋

---
+++ src/main/java/com/example/springadd/SpringaddApplication.java
@@ -0,0 +1,13 @@
+package com.example.springadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springadd/SpringaddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
